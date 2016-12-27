7748aaf435930d0bcfe2f707e4652b01a83cb13a
Julio B <tzoulio13@gmail.com>
2016-12-28 01:59:04 +0200

    GUI improvements. Image preview on side panel.

--- AAEergasia1/Form1.Designer.cs
+++ AAEergasia1/Form1.Designer.cs
@@ -24,33 +24,63 @@
         /// </summary>
         private void InitializeComponent() {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.nextBtn = new System.Windows.Forms.Button();
+            this.controlsPanel = new System.Windows.Forms.Panel();
             this.previousBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.mainPicture = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.panel1.SuspendLayout();
+            this.controlsPanel.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicture)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.panel1.Controls.Add(this.previousBtn);
-            this.panel1.Controls.Add(this.nextBtn);
+            this.panel1.Controls.Add(this.controlsPanel);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 416);
+            this.panel1.Location = new System.Drawing.Point(0, 510);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(863, 39);
+            this.panel1.Size = new System.Drawing.Size(967, 52);
             this.panel1.TabIndex = 0;
+            // 
+            // controlsPanel
+            // 
+            this.controlsPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.controlsPanel.Controls.Add(this.previousBtn);
+            this.controlsPanel.Controls.Add(this.nextBtn);
+            this.controlsPanel.Location = new System.Drawing.Point(278, 3);
+            this.controlsPanel.Name = "controlsPanel";
+            this.controlsPanel.Size = new System.Drawing.Size(379, 46);
+            this.controlsPanel.TabIndex = 2;
+            // 
+            // previousBtn
+            // 
+            this.previousBtn.Location = new System.Drawing.Point(141, 12);
+            this.previousBtn.Name = "previousBtn";
+            this.previousBtn.Size = new System.Drawing.Size(38, 23);
+            this.previousBtn.TabIndex = 1;
+            this.previousBtn.Text = "<";
+            this.previousBtn.UseVisualStyleBackColor = true;
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(194, 12);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(41, 23);
+            this.nextBtn.TabIndex = 0;
+            this.nextBtn.Text = ">";
+            this.nextBtn.UseVisualStyleBackColor = true;
             // 
             // menuStrip1
             // 
@@ -58,64 +88,9 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(863, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(967, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
-            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(863, 392);
-            this.splitContainer1.SplitterDistance = 152;
-            this.splitContainer1.TabIndex = 2;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::AAEergasia1.Properties.Resources.no_image;
-            this.pictureBox1.Location = new System.Drawing.Point(122, 36);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(467, 258);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(122, 322);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(467, 49);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "keimeno....................................................................";
-            // 
-            // nextBtn
-            // 
-            this.nextBtn.Location = new System.Drawing.Point(394, 8);
-            this.nextBtn.Name = "nextBtn";
-            this.nextBtn.Size = new System.Drawing.Size(41, 23);
-            this.nextBtn.TabIndex = 0;
-            this.nextBtn.Text = ">";
-            this.nextBtn.UseVisualStyleBackColor = true;
-            // 
-            // previousBtn
-            // 
-            this.previousBtn.Location = new System.Drawing.Point(350, 8);
-            this.previousBtn.Name = "previousBtn";
-            this.previousBtn.Size = new System.Drawing.Size(38, 23);
-            this.previousBtn.TabIndex = 1;
-            this.previousBtn.Text = "<";
-            this.previousBtn.UseVisualStyleBackColor = true;
             // 
             // fileToolStripMenuItem
             // 
@@ -131,6 +106,7 @@
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
             this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.saveAsToolStripMenuItem.Text = "Open Files..";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // saveAsToolStripMenuItem1
             // 
@@ -138,24 +114,72 @@
             this.saveAsToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
             this.saveAsToolStripMenuItem1.Text = "Save As..";
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.splitContainer1.Panel1.SizeChanged += new System.EventHandler(this.splitContainer1_Panel1_SizeChanged);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.mainPicture);
+            this.splitContainer1.Size = new System.Drawing.Size(967, 486);
+            this.splitContainer1.SplitterDistance = 170;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.richTextBox1.Location = new System.Drawing.Point(158, 369);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(467, 49);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "keimeno....................................................................";
+            // 
+            // mainPicture
+            // 
+            this.mainPicture.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.mainPicture.Image = global::AAEergasia1.Properties.Resources.no_image;
+            this.mainPicture.Location = new System.Drawing.Point(158, 82);
+            this.mainPicture.Name = "mainPicture";
+            this.mainPicture.Size = new System.Drawing.Size(467, 258);
+            this.mainPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mainPicture.TabIndex = 0;
+            this.mainPicture.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(863, 455);
+            this.ClientSize = new System.Drawing.Size(967, 562);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(500, 500);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
+            this.controlsPanel.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicture)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,13 +190,15 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox mainPicture;
         private System.Windows.Forms.Button previousBtn;
         private System.Windows.Forms.Button nextBtn;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel controlsPanel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- AAEergasia1/Form1.cs
+++ AAEergasia1/Form1.cs
@@ -10,8 +10,63 @@ using System.Windows.Forms;
 
 namespace AAEergasia1 {
     public partial class Form1 : Form {
+        private sidePanelPics side;
         public Form1() {
             InitializeComponent();
+            side = new sidePanelPics(splitContainer1.Panel1);
         }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e) {
+            openFileDialog1.ShowDialog();
+            side.loadPics(openFileDialog1.FileNames);
+            foreach(var p in side.pics) {
+                p.MouseClick += openSidePic;
+            }
+        }
+
+        private void splitContainer1_Panel1_SizeChanged(object sender, EventArgs e) {
+            if(side!=null) side.update();
+            splitContainer1.Panel1.PerformLayout();
+        }
+
+        private void openSidePic(object sender, MouseEventArgs e) {
+            var p = sender as Pics;
+            mainPicture.Image = p.Image;
+        }
+    }
+
+    class sidePanelPics {
+        public List<Pics> pics; 
+        private Panel sidePanel;
+        public sidePanelPics(Panel pan, string[] picNames = null) {
+            sidePanel = pan;
+            pics = new List<Pics> { }; 
+        }
+
+        public void loadPics(string[] picNames) {
+            foreach(string s in picNames) {
+                pics.Add(new AAEergasia1.Pics(s));
+                sidePanel.Controls.Add(pics.Last());
+            }
+            update();
+        }
+
+        public void update() {
+            int Width = sidePanel.Size.Width - 35;
+            int Height = Width;
+            for (int i = 0; i < pics.Count; i++) {
+                pics[i].Size = new Size(Width, Width);
+                pics[i].Top = 10 + (Height + 10) * i;
+                pics[i].Left = 10;
+            }
+        }
+
+    }
+
+    class Pics : PictureBox {
+        public Pics(string str) : base() {
+            Image = new Bitmap(str);
+            SizeMode = PictureBoxSizeMode.StretchImage;
+        } 
     }
 }
